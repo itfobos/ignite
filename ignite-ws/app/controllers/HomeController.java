6ff4d4137c842abfd8e2c62e93062163b7a0275b
ILya Turukin <tr.wolfer@gmail.com>
2017-08-17 13:19:05 +0700

    Useless code is removed.

--- ignite-ws/app/controllers/HomeController.java
+++ ignite-ws/app/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import play.libs.Json;
 import play.mvc.*;
 
 /**
@@ -7,15 +8,11 @@ import play.mvc.*;
  * to the application's home page.
  */
 public class HomeController extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result index() {
-        return ok(views.html.index.render());
+        Object sampleObj = new Object() {
+            public String name = "Slim Shady";
+        };
+        return ok(Json.toJson(sampleObj));
     }
 
 }
